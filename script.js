6181a4d2b4456c2bf244469bf6f3debb2cbd6b8e
Elkan DSilva <elkandsilva@elkans-mbp.home>
2023-06-08 03:39:51 -0400

    Finalized code

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let playerScore = 0,
     computerScore = 0,
     winningScore = 5; //defined opening scores + the score it takes to win the game.
@@ -15,21 +14,40 @@ let computerChoice = function() { //function for the computer to choose an item 
     const choice = ['rock', 'paper', 'scissors'];
     let computerClick = choice[Math.floor(Math.random() * choice.length)];
     console.log("computer choice: " + computerClick); //logging into console to test working status
+    document.getElementById('computerSelectionText').innerHTML = "Computer Selection: " + computerClick;
     return computerClick;
 }
 
 const compareRound = function(playerClick, computerClick) {
-    if (playerClick === computerClick){
-        console.log("You tied! Play another round.");
+    //removing highlights from computer btns
+    let cBtnRock = document.getElementById('cBtnRock');
+    let cBtnPaper = document.getElementById('cBtnPaper');
+    let cBtnScissors = document.getElementById('cBtnScissors');
+
+    cBtnRock.classList.remove('highlightComputer');
+    cBtnPaper.classList.remove('highlightComputer');
+    cBtnScissors.classList.remove('highlightComputer');
+
+    if(computerClick === 'rock'){
+        cBtnRock.classList.add('highlightComputer');
     }
-    else if(
+    else if(computerClick === 'paper'){
+        cBtnPaper.classList.add('highlightComputer');
+    }
+    else if (computerClick === 'scissors'){
+        cBtnScissors.classList.add('highlightComputer');
+    }
+
+    if (playerClick === computerClick) {
+        console.log("You tied! Play another round.");
+    } else if (
         (playerClick === "rock" && computerClick === "scissors") ||
         (playerClick === "paper" && computerClick === "rock") ||
         (playerClick === "scissors" & computerClick === "paper")
-    ){
+    ) {
         console.log("You won! Play another round.");
         playerScore++;
-    }else{
+    } else {
         console.log("The computer won. Play another round.");
         computerScore++;
     }
@@ -42,24 +60,57 @@ function playerChoice(event) {
         console.error("Invalid event object or missing target property.");
         return;
     }
+    console.log(event.target.parentNode); //Used to test the return value upon button clic.
 
-    let playerClick = event.target.id;
+    let playerClick = event.target.parentNode.id;
     let computerClick = computerChoice();
+
+    document.getElementById("playerSelectionText").innerHTML = "Player Selection: " + playerClick;
 
     console.log("User Choice: " + playerClick);
     compareRound(playerClick, computerClick);
-    
+
+    let playerSelectFlash = event.target.parentNode;
+    playerSelectFlash.classList.add('flash-animation');
+
+    setTimeout(function(){
+        playerSelectFlash.classList.remove('flash-animation');
+    },1000);
+
+    document.getElementById('scoreBoardPlayer').innerHTML = "Player score: " + playerScore;
+    document.getElementById('scoreBoardComputer').innerHTML = "Computer score: " + computerScore;
+
+
     if (playerScore === winningScore) {
         console.log("You beat the computer!");
+        document.getElementById("winnerText").innerHTML = "You beat the computer! Scores have been reset. Play again!";
         resetGame();
+
+
+        setTimeout(function(){
+            document.getElementById("winnerText").innerHTML = "";
+        }, 3000);
+
     } else if (computerScore === winningScore) {
         console.log("The computer won!");
+        document.getElementById("winnerText").innerHTML = "The computer won. Scores have been reset. Play again!";
         resetGame();
+
+        setTimeout(function(){
+            document.getElementById("winnerText").innerHTML = "";
+        }, 5500);
     }
 }
 
-let resetGame = function(){
+let resetGame = function() {
     playerScore = 0;
     computerScore = 0;
     console.log("Scores are reset to zero. Play again!");
+
+    setTimeout(function(){
+        document.getElementById("winnerText").innerHTML = "";
+    }, 3000);
+    document.getElementById('scoreBoardPlayer').innerHTML = "Player Score: ";
+    document.getElementById('scoreBoardComputer').innerHTML = "Computer Score: ";
+
 }
